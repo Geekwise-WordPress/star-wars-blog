136bacfe4bc989e2666e0d015f1c7dd24ca4d7c5
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-01-10 15:53:48 -0800

    adding profile picture and adjusting to fit

--- wordpress/wp-content/themes/wordpress-theme-starter-master/loop.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/loop.php
@@ -1,4 +1,8 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php 
+	/*	Template Name: Latest Posts
+		Template Post Type: Post */
+	if (have_posts()): while (have_posts()) : the_post(); ?>
+
 
 <!-- Dynamically adding alt text to variable -->
 		<?php $thumbnail_id  = get_post_thumbnail_id( $post->ID ); ?>
@@ -30,14 +34,25 @@
 		</div>
 	</article>
 	<!-- /article -->
-		<div class='padding py-0'>
+		<div class="grid">
 			<!-- post details -->
-			<span class="post-info byline-author"><?php _e( 'By:', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+			<div class="grid-temp">
+				<div class="item1">
+					<span class="post-info byline-author"><?php _e( 'By:', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
 
-			<div class="post-info byline-date d-flex">
-				<?php the_category();?>&nbsp// <?php the_time('F j, Y'); ?>
+					<div class="post-info byline-date d-flex">
+						<?php the_category();?>&nbsp// <?php the_time('F j, Y'); ?>	
+					</div>	
+				</div>			  
+				<!-- /post details -->
+
+				<!-- Code to use the Advanced Custom Fields Image -->
+				<div class="item2">
+				<?php if( get_field('profile_picture') ): ?>
+				<img class="profile-picture" src="<?php the_field('profile_picture'); ?>" />
+				<?php endif; ?>
+				</div>
 			</div>
-			<!-- /post details -->
 		</div>
 <?php endwhile; ?>
 
